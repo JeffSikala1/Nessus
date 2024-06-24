2ad8900ac6b32da51aa98c03789a162cc7b27bb6
Jeff Sikala <sikala9@hotmail.com>
2024-06-24 12:04:44 -0400

    chore: Update DeployNessusToVMs.sh script to install Nessus Agent on Windows and Linux VMs

--- DeployNessusToVMs.sh
+++ DeployNessusToVMs.sh
@@ -1,26 +1,46 @@
 #!/bin/bash
 
-# Connect to Azure
-az login
+# Check if jq is installed
+if ! command -v jq &> /dev/null
+then
+    echo "jq could not be found. Please install jq before running this script."
+    exit 1
+fi
 
-# Define the resource group and VMs
-resourceGroup="NessusVMsResourceGroup"
-vmNames=("NessusVM1" "NessusVM2" "NessusVM3")
+# Storage account and container details
+storageAccountName="sikalanessussa"
+storageContainerName="nessus-binary"
 
-# Nessus Agent URL
-nessusAgentUrl="https://www.tenable.com/downloads/api/v1/public/pages/nessus-agents/downloads/23248/download?i_agree_to_tenable_license_agreement=true"
-downloadPath="/tmp"
+# Generate SAS token for the storage account
+sas_token=$(az storage account generate-sas \
+    --account-name "$storageAccountName" \
+    --permissions rl \
+    --resource-types sco \
+    --services b \
+    --expiry $(date -u -d '1 day' +%Y-%m-%dT%H:%MZ) \
+    --output tsv)
+
+export AZURE_STORAGE_SAS_TOKEN=$sas_token
+export AZURE_STORAGE_ACCOUNT=$storageAccountName
+
+# Nessus Agent filenames
+nessusAgentWindows="NessusAgent-10.6.4-x64.msi"
+nessusAgentUbuntu="NessusAgent-10.6.4-ubuntu1404_amd64.deb"
+nessusAgentRHEL="NessusAgent-10.6.4-el8.x86_64.rpm"
 
 # Function to install Nessus Agent on Windows VM
 install_nessus_agent_windows() {
     vmName=$1
-    installScriptPath="${downloadPath}/InstallNessusAgent.ps1"
+    resourceGroup=$2
 
     # Create the InstallNessusAgent.ps1 script content
-    cat <<EOT > "$installScriptPath"
-Invoke-WebRequest -Uri '$nessusAgentUrl' -OutFile '$downloadPath\\NessusAgent.msi'
-Start-Process msiexec.exe -ArgumentList '/i $downloadPath\\NessusAgent.msi /quiet' -Wait
+    installScriptContent=$(cat <<EOT
+powershell -Command "Invoke-WebRequest -Uri 'https://$storageAccountName.blob.core.windows.net/$storageContainerName/$nessusAgentWindows?$sas_token' -OutFile 'C:\\Users\\nessus\\$nessusAgentWindows'; Start-Process msiexec.exe -ArgumentList '/i C:\\Users\\nessus\\$nessusAgentWindows /quiet /norestart' -Wait"
 EOT
+)
+
+    # Escape JSON characters
+    installScriptContent=$(echo "$installScriptContent" | sed 's/\\/\\\\/g' | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g')
 
     # Use Custom Script Extension to execute the script
     az vm extension set \
@@ -29,30 +49,97 @@ EOT
         --name CustomScriptExtension \
         --publisher Microsoft.Compute \
         --version 1.10 \
-        --settings "{\"fileUris\": [\"$installScriptPath\"]}" \
-        --protected-settings "{\"commandToExecute\": \"powershell -ExecutionPolicy Unrestricted -File C:\\\\Temp\\\\InstallNessusAgent.ps1\"}"
+        --settings "{\"commandToExecute\": \"$installScriptContent\"}"
 }
 
-# Function to install Nessus Agent on Linux VM
-install_nessus_agent_linux() {
+# Function to install Nessus Agent on Ubuntu VM
+install_nessus_agent_ubuntu() {
     vmName=$1
-    sshUser="Nessus"
-    privateKeyPath="/Users/jeff/.ssh/id_rsa"
+    resourceGroup=$2
 
-    # Get private IP address for the VM
-    vmPrivateIP=$(az vm nic show --resource-group "$resourceGroup" --vm-name "$vmName" --nic "$vmName-nic" --query "ipConfigurations[0].privateIpAddress" -o tsv)
+    # Create the install script content
+    installScriptContent=$(cat <<EOT
+#!/bin/bash
+wget -O /tmp/$nessusAgentUbuntu "https://$storageAccountName.blob.core.windows.net/$storageContainerName/$nessusAgentUbuntu?$sas_token"
+sudo dpkg -i /tmp/$nessusAgentUbuntu
+EOT
+)
 
-    # Commands to download and install Nessus Agent
-    ssh -i "$privateKeyPath" "$sshUser@$vmPrivateIP" "wget -O /tmp/NessusAgent.sh $nessusAgentUrl && sudo bash /tmp/NessusAgent.sh"
+    # Escape JSON characters
+    installScriptContent=$(echo "$installScriptContent" | sed 's/\\/\\\\/g' | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g')
+
+    # Use Custom Script Extension to execute the script
+    az vm extension set \
+        --resource-group "$resourceGroup" \
+        --vm-name "$vmName" \
+        --name CustomScript \
+        --publisher Microsoft.Azure.Extensions \
+        --version 2.0 \
+        --settings "{\"commandToExecute\": \"$installScriptContent\"}"
 }
 
-# Loop through the VMs and install Nessus Agent
-for vmName in "${vmNames[@]}"; do
-    osType=$(az vm get-instance-view --resource-group "$resourceGroup" --name "$vmName" --query "storageProfile.osDisk.osType" -o tsv)
+# Function to install Nessus Agent on RHEL VM
+install_nessus_agent_rhel() {
+    vmName=$1
+    resourceGroup=$2
 
-    if [ "$osType" == "Windows" ]; then
-        install_nessus_agent_windows "$vmName"
-    elif [ "$osType" == "Linux" ]; then
-        install_nessus_agent_linux "$vmName"
-    fi
+    # Create the install script content
+    installScriptContent=$(cat <<EOT
+#!/bin/bash
+sudo yum install -y wget
+sudo yum install -y rpm
+wget -O /tmp/$nessusAgentRHEL "https://$storageAccountName.blob.core.windows.net/$storageContainerName/$nessusAgentRHEL?$sas_token"
+sudo rpm -ivh /tmp/$nessusAgentRHEL
+EOT
+)
+
+    # Escape JSON characters
+    installScriptContent=$(echo "$installScriptContent" | sed 's/\\/\\\\/g' | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g')
+
+    # Use Custom Script Extension to execute the script
+    az vm extension set \
+        --resource-group "$resourceGroup" \
+        --vm-name "$vmName" \
+        --name CustomScript \
+        --publisher Microsoft.Azure.Extensions \
+        --version 2.0 \
+        --settings "{\"commandToExecute\": \"$installScriptContent\"}"
+}
+
+# Get list of all subscriptions
+subscriptions=$(az account list --query "[?state=='Enabled'].id" -o tsv)
+
+# Loop through each subscription
+for subscription in $subscriptions; do
+    az account set --subscription "$subscription"
+
+    # Get list of all VMs in the subscription
+    vms=$(az vm list --query "[].{name:name, resourceGroup:resourceGroup, osType:storageProfile.osDisk.osType}" -o json)
+
+    # Loop through the VMs and install Nessus Agent
+    for vm in $(echo "$vms" | jq -c '.[]'); do
+        vmName=$(echo "$vm" | jq -r '.name')
+        resourceGroup=$(echo "$vm" | jq -r '.resourceGroup')
+        osType=$(echo "$vm" | jq -r '.osType')
+
+        echo "Processing VM: $vmName, Resource Group: $resourceGroup, OS Type: $osType"
+
+        if [ "$osType" == "Windows" ]; then
+            install_nessus_agent_windows "$vmName" "$resourceGroup"
+        elif [ "$osType" == "Linux" ]; then
+            # Check if Ubuntu or RHEL
+            osInfo=$(az vm run-command invoke -g "$resourceGroup" -n "$vmName" \
+                --command-id RunShellScript \
+                --scripts "cat /etc/*release" \
+                --query "value[0].message" -o tsv | tr -d '\r')
+
+            if [[ "$osInfo" == *"Ubuntu"* ]]; then
+                install_nessus_agent_ubuntu "$vmName" "$resourceGroup"
+            elif [[ "$osInfo" == *"Red Hat"* ]]; then
+                install_nessus_agent_rhel "$vmName" "$resourceGroup"
+            else
+                echo "Unsupported or unknown Linux distribution for VM: $vmName"
+            fi
+        fi    
+    done
 done
